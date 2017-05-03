fe28374d0a182a322eb6cf25f854386d23423f77
kkorolyov <kirillk753@gmail.com>
2017-05-03 15:19:14 -0700

    Update syncGit

--- syncGit.sh
+++ syncGit.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-git -C ~/Documents/Money pull
-git -C ~/Documents/School/SJSU pull
+sources=(
+	~/Documents/Money
+	~/Documents/School/SJSU
+)
 
+for source in ${sources[@]}; do
+	echo $source
+
+	git -C $source pull
+
+	git -C $source add .
+	git -C $source commit -m "Sync `date +'%Y-%m-%d'`"
+	git -C $source push
+done
