822261bbe47d1f1957189935ab8369d6a8753f57
kkorolyov <kirillk753@gmail.com>
2017-05-03 20:51:19 -0700

    Fix bat

--- syncGit.bat
+++ syncGit.bat
@@ -1,21 +1,16 @@
 @echo off
 set git="C:\Tools\Git\bin\git.exe"
-set sources=(
-	C:\Documents\Money
-	C:\Documents\School\SJSU
-)
-@echo on
+set sources=C:\Documents\Money C:\Documents\School\SJSU
+
 for %%a in (%sources%) do (
 	echo %%a
 
 	%git% -C %%a pull
-
+	
 	%git% -C %%a add .
 	set msg='date +"%%Y-%%m-%%d"'
 	%git% -C %%a commit -m "Sync %msg%"
 	%git% -C %%a push
 )
-@echo off
 pause
 exit /b %ERRORLEVEL%
-
