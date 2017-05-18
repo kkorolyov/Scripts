a9e79784b2dbbfa9db2514bbdc38b2273dce501b
kkorolyov <kirillk753@gmail.com>
2017-05-17 18:16:23 -0700

    Remove full git path from syncGit.bat

--- syncGit.bat
+++ syncGit.bat
@@ -1,16 +1,16 @@
 @echo off
-set git="C:\Tools\Git\bin\git.exe"
 set sources=C:\Documents\Money C:\Documents\School\SJSU
 
 for %%a in (%sources%) do (
 	echo %%a
 
-	%git% -C %%a pull
+	git -C %%a pull
 	
-	%git% -C %%a add .
+	git -C %%a add .
 	set msg='date +"%%Y-%%m-%%d"'
-	%git% -C %%a commit -m "Sync %msg%"
-	%git% -C %%a push
+	git -C %%a commit -m "Sync %msg%"
+	git -C %%a push
 )
 pause
 exit /b %ERRORLEVEL%
+
